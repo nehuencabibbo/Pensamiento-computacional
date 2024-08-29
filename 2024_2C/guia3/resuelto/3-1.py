916ec555f4905a423d6fd0244c5756e08dae9211
nehuencabibbo <ncabibbo@fi.uba.ar>
2024-08-29 15:08:40 -0300

    Borro resueltos previos guia 3

--- 2024_2C/guia3/resuelto/3-1.py
+++
@@ -1,5 +0,0 @@
-def imprimir_entre(inicio, fin):
-  for i in range(inicio, fin + 1):
-    print(i)
-
-imprimir_entre(10, 20)
--- 2024_2C/guia3/resuelto/3-9.py
+++
@@ -1,8 +0,0 @@
-def es_primo(num):
-  for i in range(2, num):
-    if num % i == 0:
-      return False 
-    
-  return True
-
-es_primo(49979687)
